2f7c42eded06076ae3f20aa5d0a10a83156e5666
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2022-03-18 14:12:22 +0100

    Remove empty script

--- scripts/rename_dates.jl
+++
@@ -1,4 +0,0 @@
-# Motivation: Pia wanted me to append dates to the FASTA files from swine samples.
-# If sample date is not available, instead append recievedate.
-# Instead of modifying the pipeline, it's easier to rename the files after the fact.
-# function read_datefile(io::IO, samples::Set{String})
